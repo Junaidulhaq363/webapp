b57a2342eaafb61632a8cbf0de2d2a9364a0b557
Junaidulhaq363 <66385932+Junaidulhaq363@users.noreply.github.com>
2021-11-05 16:12:11 +0530

    Add files via upload

---
+++ HelloController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace webapp.Controllers
+{
+    public class HelloController : Controller
+    {
+        // 
+        // GET: /Hello/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /Hello/Welcome/ 
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
