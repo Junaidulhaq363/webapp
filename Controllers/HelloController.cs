5ea77148e62000e60ebd2edd2344eb813c31da01
Junaid <haqjunaidul5@gmail.com>
2021-11-05 00:09:21 -0700

    Initial commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace webapp.Controllers
+{
+    public class HelloController : Controller
+    {
+        // 
+        // GET: /Hello/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /Hello/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
